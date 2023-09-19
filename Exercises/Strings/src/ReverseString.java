18ad93505e17024e9889f0faf9d6955219ab4dd0
Justinshakes <Superjustin44@gmail.com>
2023-09-19 01:49:22 -0700

    Reverse A String

--- Exercises/Strings/src/ReverseString.java
+++ Exercises/Strings/src/ReverseString.java
@@ -1,5 +1,19 @@
 public class ReverseString {
     public static void main(String[] args) {
-        
+        System.out.println(reverseString("Hello World"));
+
+
+    }
+
+//    public static String reverseString(String s){
+//        StringBuilder sb = new StringBuilder();
+//        for(int i = s.length() -1; i >= 0; i--) {
+//            sb.append(s.charAt(i));
+//        }
+//        return sb.toString().strip();
+//    }
+
+    public static String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
     }
 }
